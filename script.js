7c73d2f0c464f5a1ad4b9b710e290aacaca0e2e9
emma <emmahelenalindgren@gmail.com>
2017-03-21 11:07:00 +0100

    första

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function(){
+
+var autBtn = document.getElementById("autBtn");
+var logoutBtn = document.getElementById("logoutBtn");    
+    
+    
+    
+    
+});
