c2700b55a0ad8714a8eb18977678f84f875b8888
emma <emmahelenalindgren@gmail.com>
2017-03-21 11:32:06 +0100

    testing

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 window.addEventListener("load", function(){
 
+var provider = new firebase.auth.GithubAuthProvider();    
+    
 var autBtn = document.getElementById("autBtn");
 var logoutBtn = document.getElementById("logoutBtn");    
+    
+    
+firebase.auth().signInWithPopup(provider)
+.then(function(result) {
+	// Om autentisering lyckas, så finns användarinfo i user
+	let user = result.user;
+});
+    
+    
+firebase.auth().signOut()
+.then(function(result) {
+	// Utloggning lyckades
+})
+.catch(function(error) {
+	// Utloggning misslyckades
+});
+    
+    
+    
     
     
     
