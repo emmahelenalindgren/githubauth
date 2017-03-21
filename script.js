dc0f5cd6baa53d92a7928ae73b8e2f9f6dd76319
emma <emmahelenalindgren@gmail.com>
2017-03-21 11:45:04 +0100

    testar js

--- script.js
+++ script.js
@@ -4,22 +4,45 @@ var provider = new firebase.auth.GithubAuthProvider();
     
 var autBtn = document.getElementById("autBtn");
 var logoutBtn = document.getElementById("logoutBtn");    
+logoutBtn.style.display = "none";    
+var infoText = document.getElementById("infotext"); 
+var infoTextFail = document.getElementById("infotextFail");     
+infoText.innerHTML = "";    
+
+/**** log in knapp ****/
     
-    
-firebase.auth().signInWithPopup(provider)
+autBtn.addEventListener("click", function(event){
+   firebase.auth().signInWithPopup(provider)
 .then(function(result) {
 	// Om autentisering lyckas, så finns användarinfo i user
 	let user = result.user;
-});
+        
+}); 
+    autBtn.style.display = "inherit"; 
+    logoutBtn.style.display = "none"; 
+    infoText.innerHTML =  `Du är inloggad som ${user.displayName}`;
+});    
+  
     
     
+
+/**** logout knapp ****/
+       
+autBtn.addEventListener("click", function(event){
+   firebase.auth().signInWithPopup(provider)
 firebase.auth().signOut()
 .then(function(result) {
 	// Utloggning lyckades
 })
 .catch(function(error) {
 	// Utloggning misslyckades
+    infoTextFail.innerHTML = "Utloggning misslyckades";
 });
+    autBtn.style.display = "none"; 
+    logoutBtn.style.display = "inherit";  
+});       
+    
+
     
     
     
