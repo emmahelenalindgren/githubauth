a84fb27b4f5bf179ffccea8cdff68cd308c2201f
emma <emmahelenalindgren@gmail.com>
2017-03-21 12:46:43 +0100

    testar med consolelog

--- script.js
+++ script.js
@@ -1,57 +1,48 @@
-window.addEventListener("load", function(){
-
-var provider = new firebase.auth.GithubAuthProvider();    
-    
-var autBtn = document.getElementById("autBtn");
-var logoutBtn = document.getElementById("logoutBtn");    
-logoutBtn.style.display = "none";    
-var infoText = document.getElementById("infotext"); 
-var infoTextFail = document.getElementById("infotextFail");     
-   
-    
-
-/**** log in knapp ****/
-    
-autBtn.addEventListener("click", function(event){
-   firebase.auth().signInWithPopup(provider)
-.then(function(result) {
-	// Om autentisering lyckas, så finns användarinfo i user
-	let user = result.user;
-       if (user.displayName == null){   
-           infoText.innerHTML =  `Du är inloggad som ${user.email}`;
-       } else {
-          infoText.innerHTML =  `Du är inloggad som ${user.displayName}`;   
-       };     
-}); 
-    autBtn.style.display = "none"; 
-    logoutBtn.style.display = "inherit"; 
-});    
-  
+window.addEventListener("load", function() {
     
     
-
-/**** logout knapp ****/
-       
-autBtn.addEventListener("click", function(event){
-   firebase.auth().signInWithPopup(provider)
-firebase.auth().signOut()
-.then(function(result) {
-	// Utloggning lyckades
-})
-.catch(function(error) {
-	// Utloggning misslyckades
-    infoTextFail.innerHTML = "Utloggning misslyckades";
-});
-    infoText.innerHTML =  "";   
-    autBtn.style.display = "inherit"; 
-    logoutBtn.style.display = "none";  
-});       
-    
-
+	var provider = new firebase.auth.GithubAuthProvider();
+	var autBtn = document.getElementById("autBtn");
+	var logoutBtn = document.getElementById("logoutBtn");
+	var infoText = document.getElementById("infotext");
+	var infoTextFail = document.getElementById("infotextFail");
     
     
+	logoutBtn.style.display = "none";
     
     
+	/**** log in knapp ****/
+	autBtn.addEventListener("click", function(event) {
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+			// Om autentisering lyckas, så finns användarinfo i user
+			let user = result.user;
+			if (user.displayName == null) {
+				infoText.innerHTML = `Du är inloggad som ${user.email}`;
+			} else {
+				infoText.innerHTML = `Du är inloggad som ${user.displayName}`;
+			};
+		});
+		autBtn.style.display = "none";
+		logoutBtn.style.display = "inherit";
+	});
+    
+    
+	/**** logout knapp ****/
+	autBtn.addEventListener("click", function(event) {
+		firebase.auth().signInWithPopup(provider)
+		firebase.auth().signOut().then(function(result) {
+			// Utloggning lyckades
+            console.log("utloggning lyckades");
+            infoText.innerHTML = `Du är inte inloggad`;
+            
+		}).catch(function(error) {
+			console.log("utloggning misslyckades");
+			infoTextFail.innerHTML = "Utloggning misslyckades";
+		});
+		infoText.innerHTML = "";
+		autBtn.style.display = "inherit";
+		logoutBtn.style.display = "none";
+	});
     
     
     
